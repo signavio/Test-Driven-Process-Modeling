4a566d7ae6c4b893f6fe0e10978057bb71f32d96
Gowtham Mohan <gowtham.mohan@signavio.com>
2020-10-09 14:41:56 +0200

    refactor:
    * remove unneeded imports
    * use the test driven approach to validate the model before compiling to smart contract

--- app.js
+++ app.js
@@ -1,34 +1,25 @@
-const express = require('express')
-const path = require('path')
-const mongoose = require('mongoose')
-const bodyparser = require('body-parser')
-const request = require('request')
-const StringDecoder = require('string_decoder').StringDecoder
-const cookieParser = require('cookie-parser')
-const axios = require('axios')
-const qs = require('qs')
-const http = require('http')
-const https = require('https')
-const fetch = require('node-fetch')
-const { exec } = require('child_process')
-const convert = require('xml-js')
-const kill = require('kill-port')
-const preProcessingModule = require('./preProcessingModule')
-const compiler = require('bpmn-sol')
-const testModule = require('./testModule')
-const fetchTestScript = require('./testModule')
-const beautify = require('js-beautify').js
+import express from 'express'
+import { join } from 'path'
+import { urlencoded, json } from 'body-parser'
+import cookieParser from 'cookie-parser'
+import axios from 'axios'
+import { stringify } from 'qs'
+import { addGlobalDocumentation } from './preProcessingModule.js'
+import { compile } from 'bpmn-sol'
+import { hasTestPassed } from './server/testModule.js'
+import beautify from 'js-beautify'
+import path from 'path'
 
 const app = express()
 const port = process.env.PORT || 4000
 app.use(cookieParser())
 
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', join(`${path.resolve('./')}`, 'views'))
 app.set('view engine', 'pug')
 
-app.use(bodyparser.urlencoded({ extended: false }))
-app.use(bodyparser.json())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(urlencoded({ extended: false }))
+app.use(json())
+app.use(express.static(join(`${path.resolve('./')}`, 'public')))
 
 app.get('/', (req, res) => {
   res.render('home', {
@@ -63,7 +54,7 @@ app.post('/engine', async (req, res, next) => {
     var cookieData
 
     await axios
-      .post(login_url, qs.stringify(data))
+      .post(login_url, stringify(data))
       .then(function (response) {
         cookieData = response.headers['set-cookie']
         return cookieData
@@ -78,15 +69,15 @@ app.post('/engine', async (req, res, next) => {
     async function fetchXML(cookieData) {
       let temp1 = String(cookieData[0]).split(';')
       let jID = String(temp1[0]).split('=')
-      jsesssion_ID = jID[1]
+      let jsesssion_ID = jID[1]
 
       let temp2 = String(cookieData[1]).split(';')
       let tok = String(temp2[0]).split('=')
-      auth_token = tok[1]
+      let auth_token = tok[1]
 
       let temp3 = String(cookieData[4]).split(';')
       let rID = String(temp3[0]).split('=')
-      lb_route_ID = rID[1]
+      let lb_route_ID = rID[1]
 
       let diagram_url =
         base_url + '/p/revision' + '/' + revision_ID + '/' + format
@@ -117,40 +108,33 @@ app.post('/engine', async (req, res, next) => {
           console.log(error)
         })
 
-      let xmlWithGlobalVariables = preProcessingModule.addGlobalDocumentation(
+      let xmlWithGlobalVariables = addGlobalDocumentation(
         xmlDiagram,
         globalVariables,
         contractName
       )
-      /**
-       * Commenting out the test driven approach until we support the testing of multiple script tasks. Once the support is implemented, the following line should be uncommented.
-       */
-      // let successFlag = testModule.testModule(xmlDiagram)
-      let successFlag = true
-
+      const successFlag = hasTestPassed(xmlWithGlobalVariables)
       if (successFlag) {
-        const contract = compiler
-          .compile(xmlWithGlobalVariables)
-          .then((contract) => {
-            console.log(
-              'Solidity Smart Contract:',
-              beautify(contract.Solidity, {
-                indent_size: 2,
-                space_in_empty_paren: true,
-              })
-            )
-            console.log(
-              '--------------------------------------------------------------------------------------------------------------------------------------------------------'
-            )
-            console.log(
-              'ABI:',
-              beautify(contract.ABI, {
-                indent_size: 2,
-                space_in_empty_paren: true,
-              })
-            )
-            res.redirect('/result')
-          })
+        const contract = compile(xmlWithGlobalVariables).then((contract) => {
+          console.log(
+            'Solidity Smart Contract:',
+            beautify(contract.Solidity, {
+              indent_size: 2,
+              space_in_empty_paren: true,
+            })
+          )
+          console.log(
+            '--------------------------------------------------------------------------------------------------------------------------------------------------------'
+          )
+          console.log(
+            'ABI:',
+            beautify(contract.ABI, {
+              indent_size: 2,
+              space_in_empty_paren: true,
+            })
+          )
+          res.redirect('/result')
+        })
       } else {
         res.redirect('/error')
       }

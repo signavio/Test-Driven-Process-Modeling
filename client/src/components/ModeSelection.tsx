ac950fa4adbc471bfcfc2baad2a9a418eaa2a145
Gowtham Mohan <gowtham.mohan@signavio.com>
2020-10-30 16:25:48 +0100

    style: adjust margin of modeSelection container

--- client/src/components/ModeSelection.tsx
+++ client/src/components/ModeSelection.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 const Container = styled.div`
 display: flex;
 flex-direction: column;
-margin-top: 100px;
+margin-top: 150px;
 justify-content:baseline;
 height: 40%;
 align-items: center;

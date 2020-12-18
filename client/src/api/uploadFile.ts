47747d5337a5d36d8c51ae5ec5a0ed615b9fee12
Gowtham Mohan <gowtham.mohan@signavio.com>
2020-12-18 15:31:50 +0100

    fix: adjust shape of response

--- client/src/api/uploadFile.ts
+++ client/src/api/uploadFile.ts
@@ -1,16 +1,18 @@
 import axios from 'axios'
 
 type CustomResponse = {
-    status: number,
-    message: string,
-    data?: string
+    data: {
+        data: string,
+        status: number,
+        message: string,
+    }
 }
 const uploadFile = async (data: FormData) => {
     try {
         const response: CustomResponse = await axios.post('/file',
             data
         )
-        return response
+        return response.data
     } catch (error) {
         throw error
     }

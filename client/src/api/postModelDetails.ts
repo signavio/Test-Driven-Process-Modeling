22532a6f5b7904411d1cc183860afab316e988fb
Gowtham Mohan <gowtham.mohan@signavio.com>
2020-12-18 15:20:28 +0100

    feat: implement a handler to post model details to compile endpoint

---
+++ client/src/api/postModelDetails.ts
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+type RequestData = {
+    globalVariables: string,
+    contractName: string
+}
+const postModelDetails = async (data: RequestData) => {
+    try {
+        const response = await axios('/compile', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            data: JSON.stringify(data)
+        })
+
+        return response
+    } catch (error) {
+        throw error
+    }
+}
+
+export default postModelDetails 
